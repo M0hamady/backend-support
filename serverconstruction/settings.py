f6c8bbbb4d18878346730070f02249031b726eca
M0hamady <mohammedmohammedy96@gmail.com>
2023-01-20 14:24:20 +0200

    time zone

--- serverconstruction/settings.py
+++ serverconstruction/settings.py
@@ -131,14 +131,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-TIME_ZONE = 'Africa/Egypt'
+# TIME_ZONE = 'Africa/Egypt'
 
 
 # Static files (CSS, JavaScript, Images)
