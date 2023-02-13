a722b0c59fdee0b0a47fb6fd60080bfc5014af0d
M0hamady <119875626+M0hamady@users.noreply.github.com>
2023-02-13 18:29:55 +0200

    update csrf

--- serverconstruction/urls.py
+++ serverconstruction/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.urlpatterns import format_suffix_patterns
-from django.views.decorators.csrf import csrf_exempt
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('meeting/', include('meets.urls')),
